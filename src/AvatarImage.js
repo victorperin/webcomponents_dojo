5b82a7b0cafce90fb811bcee74350a8361d6dd60
Victor Perin <me@victorperin.com>
2015-05-26 10:51:17 -0300

    simplificando o código

--- src/AvatarImage.js
+++ src/AvatarImage.js
@@ -1,15 +1,18 @@
-'use strict';
+'use strict'; //strict mode (aprenda aqui: http://www.w3schools.com/js/js_strict.asp )
 
-class AvatarImage extends HTMLImageElement {
+class AvatarImage extends HTMLImageElement { //ES6 classes (Orientação a objetos!!!) (Funciona apenas no chrome por enquanto)
+  //cria uma classe extendida de HTMLImageElement
     createdCallback() {
         var self = this,
-            nome = this.getAttribute('nome');
+            nome = this.getAttribute('nome'); //procura pelo atributo nome no custom element
 
-        if (nome) {var url = 'avatar/' + encodeURI(nome) + ".jpg";}
-        else {throw new Error('Username attribute is required.');}
-        var shadow = this.createShadowRoot();
-        var image = shadow.appendChild(document.createElement('img'));
-        image.setAttribute('src', url);
+        if (!nome){throw new Error('Username attribute is required.');}
+        else {
+          var url = 'avatar/' + encodeURI(nome) + ".jpg";
+          var shadow = this.createShadowRoot();
+          var image = shadow.appendChild(document.createElement('img'));
+          image.setAttribute('src', url);
+        }
     }
 }
 
