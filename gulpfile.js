9a6341d4f4719938bbc9a2fca880c7fc368cd5d1
Victor Perin <me@victorperin.com>
2015-05-26 10:29:51 -0300

    comentando o gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp = require('gulp'),
-    babel = require('gulp-babel'),
-    connect = require('gulp-connect'),
-    uglify = require('gulp-uglify');
+var gulp = require('gulp'), // https://www.npmjs.com/package/gulp
+    babel = require('gulp-babel'), // https://www.npmjs.com/package/gulp-babel
+    connect = require('gulp-connect'), // https://www.npmjs.com/package/gulp-connect
+    uglify = require('gulp-uglify'); // https://www.npmjs.com/package/gulp-uglify
 
-gulp.task('dist', function () {
-    return gulp.src('src/*')
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+gulp.task('dist', function () { //rode isso com o comando 'gulp dist'
+    return gulp.src('src/*') //pega tudo o que estiver na pasta src
+        .pipe(babel()) //torna o script compativel com ES5
+        .pipe(uglify()) //minifica o script
+        .pipe(gulp.dest('dist')); //salva na pasta dist
 });
 
 gulp.task('server', function() {
